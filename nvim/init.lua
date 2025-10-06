923bd7c698cdd176bebf76432bb3f2057da0a5d8
Joe <joseph.palmer@medshr.net>
2025-10-06 18:18:04 +0100

    Updated with Shortcuts

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,43 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
 require("user.options")
 require("config.lazy")
 require("user.colorscheme")
+
+
+-- telescope setup --
+require('telescope').setup({
+  defaults = {
+    -- Disable treesitter for previews to fix the ft_to_lang error
+    preview = {
+      treesitter = false
+    },
+  },
+})
+
+
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>ff', function()
+  builtin.find_files({
+    search_dirs = {
+      "~/Dev", 
+      "~/Documentation",
+      "~/PersonalProjects",
+      "~/.config",
+      "~/RustroverProjects",
+      "~/Tools"
+      -- Add other specific directories you want to search
+    }
+  })
+end)
+
+vim.keymap.set('n', '<leader>fl', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
