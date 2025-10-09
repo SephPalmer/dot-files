f318d74e2e225bc3e5538414cbf2679ffc5a898d
Joe <joseph.palmer@medshr.net>
2025-10-09 16:33:08 +0100

    more plugins

--- nvim/lua/user/config/lsp.lua
+++ nvim/lua/user/config/lsp.lua
@@ -1,26 +1,99 @@
 return {
-	{
+  {
     "mason-org/mason.nvim",
     opts = {
-        ui = {
-            icons = {
-                package_installed = "✓",
-                package_pending = "➜",
-                package_uninstalled = "✗"
-            }
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
         }
+      }
     }
-    },
-    
-    {
+  },
+  
+  {
     "mason-org/mason-lspconfig.nvim",
-    opts = {
-        ensure_installed = { "lua_ls", "rust_analyzer" },
-	automatic_installation = true
-    },
     dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
+      "mason-org/mason.nvim",
     },
-}
+    opts = {
+      ensure_installed = { "lua_ls", "rust_analyzer" },
+      automatic_installation = true,
+    },
+  },
+
+  -- LSP configuration
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "mason-org/mason-lspconfig.nvim" },
+    config = function()
+      -- Keybindings for LSP
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          local bufnr = args.buf
+          local opts = { buffer = bufnr, noremap = true, silent = true }
+          
+          -- Navigation
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
+          
+          -- Documentation
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+          
+          -- Refactoring
+          vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+          vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+          
+          -- Diagnostics
+          vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+          vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+          vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+          vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+          
+          -- Formatting
+          vim.keymap.set('n', '<leader>f', function()
+            vim.lsp.buf.format({ async = true })
+          end, opts)
+        end,
+      })
+
+      -- Configure rust-analyzer
+      vim.lsp.config('rust_analyzer', {
+        cmd = { 'rust-analyzer' },
+        root_markers = { 'Cargo.toml' },
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+            },
+            check = {
+              command = "clippy",
+            },
+          },
+        },
+      })
+
+      -- Configure lua_ls
+      vim.lsp.config('lua_ls', {
+        cmd = { 'lua-language-server' },
+        root_markers = { '.luarc.json', '.luacheckrc' },
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { 'vim' },
+            },
+          },
+        },
+      })
+
+      -- Enable the servers
+      vim.lsp.enable('rust_analyzer')
+      vim.lsp.enable('lua_ls')
+    end,
+  },
 }
